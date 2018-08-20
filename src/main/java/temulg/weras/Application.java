496865029f5298be34a87098451e47c9ddceb548
Alex Dubov <oakad@yahoo.com>
2018-08-21 00:15:06 +1000

    Skeleton app
    Signed-off-by: Alex Dubov <oakad@yahoo.com>

---
+++ src/main/java/temulg/weras/Application.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2018 Alex Dubov <oakad@yahoo.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package temulg.weras;
+
+public class Application extends javafx.application.Application {
+	public static void main(String... args) {
+		System.out.println("AaAA!");
+	}
+}
